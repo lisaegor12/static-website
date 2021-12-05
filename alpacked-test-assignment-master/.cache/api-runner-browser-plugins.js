b04203b59c3c9371875885bf66383106895815e9
lisaegor12 <95425015+lisaegor12@users.noreply.github.com>
2021-12-05 16:00:01 +0200

    was a big mistake to upload build on main branch

--- alpacked-test-assignment-master/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"John Doe","short_name":"johndoe","start_url":"/","background_color":"#ffffff","theme_color":"#3182ce","display":"minimal-ui","icon":"src/images/icon.png"},
-    }]
